54c1f9879027fbcb9a8463e760ad396722053982
liuwei <liuwei@81med.com>
2018-06-21 17:15:05 +0800

    add grouppoint

---
+++ src/datasets/GroupPoints.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import numpy as np
+import math
+import tensorflow as tf
+import SoccerDb
+import Groups
+import sys
+import copy
+
+#trainfiles='worldcup.csv'
+grouppointfile='grouppoint.csv'
+
+"""
+    {
+        2006:{
+            'A':{
+                    0:[0,0,0,0],
+                    1:[3,3,0,0],
+                    2:[6,6,0,0],
+                    3:[9,6,3,0]
+                },
+            'B':{
+                    0:[0,0,0,0],
+                    1:[3,3,0,0],
+                    2:[6,3,3,0],
+                    3:[9,4,4,0]
+                },
+            },
+        2010:{
+            'A':{
+                    0:[0,0,0,0],
+                    1:[3,3,0,0],
+                    2:[6,6,0,0],
+                    3:[9,6,3,0]
+                },
+            'B':{
+                    0:[0,0,0,0],
+                    1:[3,3,0,0],
+                    2:[6,3,3,0],
+                    3:[9,4,4,0]
+                },
+            },
+    }
+"""
+
+def GetHostGuestPts(hscore,gscore):
+    if hscore>gscore:
+        return 3,0
+    elif hscore==gscore:
+        return 1,1
+    else:
+        return 0,3
+
+class GroupsPoints():
+    def __init__(self,data_dir='../data/',Recompute=False):
+        self._data_dir = data_dir
+        self._matches = None
+        self._groups =  Groups.Groups(self._data_dir)
+        self._points = {}
+
+    def ComputePoints(self,):
+        self._matches =  SoccerDb.load_alltraindata(self._data_dir)
+        self._groups.LoadFile()
+
+        self._matches.sort_values(by=['Year','Month','Day'],inplace=True)
+        rownum = self._matches.shape[0]
+
+        year=-1
+        ground=-1
+        matchnum={}
+        for i in range(rownum):
+            year = self._matches['Year'][i]
+            host = self._matches['Host'][i]
+            guest = self._matches['Guest'][i]
+            group = self._groups.GetGroup(host,year)
+            group2 = self._groups.GetGroup(guest,year)
+            if group!=group2:
+                continue
+
+            if not self._points.has_key(year):
+                self._points[year]={}
+                matchnum[year]={}
+                groups = self._groups.GetGroupNames(year)
+                for gi in groups:
+                    matchnum[year][gi]=0
+                    self._points[year][gi]={}
+                    self._points[year][gi][0]={}
+                    for ni in self._groups.GetGroupMem(year,gi):
+                        self._points[year][gi][0][ni]=0
+            
+            ground=int(matchnum[year][group]/2)+1
+            if matchnum[year][group] %2 == 0:
+                self._points[year][group][ground]=copy.deepcopy(self._points[year][group][ground-1])
+
+            hp,gp = GetHostGuestPts(self._matches['HostGoal'][i],self._matches['GuestGoal'][i])
+            self._points[year][group][ground][host]+=hp
+            self._points[year][group][ground][guest]+=gp
+            matchnum[year][group] += 1
+    
+    def GetPoints(self,year,group,ground,T1,T2,T3):
+        return self._points[year][group][ground]
+        #return [0,0,0,0]
+
+    def PrintPoints(self):
+        for year in self._points.keys():
+            print("Year:%d"%year)
+            for group in self._points[year].keys():
+                print(" Group:%s"%group)
+                sys.stdout.write("                ")
+                for nation in self._groups.GetGroupMem(year,group):
+                   sys.stdout.write("%18s"%nation)
+                sys.stdout.write("\n")
+                for ground in self._points[year][group]:
+                    sys.stdout.write("     round %4d:"%ground)
+                    for nation in self._groups.GetGroupMem(year,group):
+                       sys.stdout.write("%18d"%self._points[year][group][ground][nation])
+                    sys.stdout.write("\n")
+        
+
+if __name__ == '__main__':
+    gp = GroupsPoints(Recompute=True)
+    gp.ComputePoints()
+
+    year=2006
+    group='A'
+    ground=2
+    M1='Germany'
+    M2='Sweden'
+    #pts = gp.GetPoints(year=year,group=group,ground=ground,T1=M1,T2=M2)
+    #print("Year:%d Group:%s Round:%d Team:%s vs %s pts="%(year,group,ground,M1,M2))
+    #print(pts) 
+
+    gp.PrintPoints()
